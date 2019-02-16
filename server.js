3502f063d14e9304ecf307693e4cc0e946f212f9
Justin White <kyjus25@gmail.com>
2019-02-16 01:57:46 -0600

    It works!

--- server.js
+++ server.js
@@ -2,73 +2,95 @@ const Alexa = require('ask-sdk');
 const GloSDK = require('@axosoft/glo-sdk')
 
 let skill;
+let boards = [];
+
+async function getBoards(authToken) {
+	await GloSDK(authToken).boards.getAll().then(res => boards = res);
+	return boards;
+}
 
 const GetBoardsHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'GetBoardsIntent';
-  },
-  handle(handlerInput) {
-    const speechText = 'Get Boards!';
-    const authToken = handlerInput.requestEnvelope.context.System.user.accessToken;
+	canHandle(handlerInput) {
+		return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+			handlerInput.requestEnvelope.request.intent.name === 'GetBoardsIntent';
+	},
+	async handle(handlerInput) {
+		const authToken = handlerInput.requestEnvelope.context.System.user.accessToken;
 
-    GloSDK(authToken).boards.getAll().then(res => {
-      console.log(res);
-    });
+		await getBoards(authToken).then(res => {
+			console.log(res);
+		});
 
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('Get Boards with token', speechText)
-      .getResponse();
-  }
+		let speechText = 'You have ' + boards.length + ' boards: ';
+
+		if (boards.length == 1) {
+			speechText += 'Your only board is ' + boards[0]['name'] + ' ';
+		} else if (boards.length > 1) {
+			speechText += 'Your boards are, ';
+			for (i = 0; i < boards.length; i++) {
+				if (i != (boards.length - 1)) {
+					speechText += boards[i]['name'] + ', ';
+				} else {
+					speechText += 'and ' + boards[i]['name'] + '.';
+				}
+			}
+		}
+
+		return handlerInput.responseBuilder
+			.speak(speechText)
+			.withSimpleCard(speechText, speechText)
+			.getResponse();
+	}
 };
 
 if (process.env.NODE_ENV === 'production') {
 
-  exports.handler = async function (event, context) {
-    if (!skill) {
-      skill = Alexa.SkillBuilders.custom()
-        .addRequestHandlers(
-          GetBoardsHandler
-        )
-        .create();
-    }
-    return skill.invoke(event,context);
-  }
+	exports.handler = async function (event, context) {
+		if (!skill) {
+			skill = Alexa.SkillBuilders.custom()
+				.addRequestHandlers(
+					GetBoardsHandler
+				)
+				.create();
+		}
+		return skill.invoke(event, context);
+	}
 
 } else {
 
-  // Development environment - we are on our local node server
-  const express = require('express');
-  const bodyParser = require('body-parser');
-  const app = express();
+	// Development environment - we are on our local node server
+	const express = require('express');
+	const bodyParser = require('body-parser');
+	const app = express();
 
-  app.use(bodyParser.json());
-  app.post('/', function(req, res) {
+	app.use(bodyParser.json());
+	app.post('/', function (req, res) {
 
-    if (!skill) {
+		console.log(req.body.context.System.user.accessToken);
 
-      skill = Alexa.SkillBuilders.custom()
-        .addRequestHandlers(
-          GetBoardsHandler
-        )
-        .create();
+		if (!skill) {
 
-    }
+			skill = Alexa.SkillBuilders.custom()
+				.addRequestHandlers(
+					GetBoardsHandler
+				)
+				.create();
 
-    skill.invoke(req.body)
-      .then(function(responseBody) {
-        res.json(responseBody);
-      })
-      .catch(function(error) {
-        console.log(error);
-        res.status(500).send('Error during the request');
-      });
+		}
 
-  });
+		skill.invoke(req.body)
+			.then(function (responseBody) {
+				res.json(responseBody);
+			})
+			.catch(function (error) {
+				console.log(error);
+				res.status(500).send('Error during the request');
+			});
 
-  app.listen(3000, function () {
-    console.log('Development endpoint listening on port 3000!');
-  });
+	});
+
+	app.listen(3000, function () {
+		console.log('Development endpoint listening on port 3000!');
+	});
 
 }
